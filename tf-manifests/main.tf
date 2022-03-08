7526a3a16c9504b325f200407a405206d4f2df82
Gopi Krishna <gopikrishna.83@gmail.com>
2022-03-08 09:47:42 +0100

    Downgraded provider version

--- tf-manifests/main.tf
+++ tf-manifests/main.tf
@@ -8,11 +8,11 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 3.0"
+      version = "~> 2.0"
     }
     azuread = {
       source  = "hashicorp/azuread"
-      version = "~> 3.0"
+      version = "~> 2.0"
     }
   }
   backend "azurerm" {}
