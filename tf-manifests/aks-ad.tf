665728c0c9ef9404fcdf4bbb76d88b90675fb923
Gopi Krishna <gopikrishna.83@gmail.com>
2022-03-08 10:31:35 +0100

    Few corrections done

--- tf-manifests/aks-ad.tf
+++ tf-manifests/aks-ad.tf
@@ -2,7 +2,7 @@
 # AKS Cluster resources
 ##########################################################
 resource "azuread_group" "aks_admins" {
-  name             = "${azurerm_resource_group.aks_rg.name}-${var.environment}-admins"
+  #name             = "${azurerm_resource_group.aks_rg.name}-${var.environment}-admins"
   display_name     = "${azurerm_resource_group.aks_rg.name}-${var.environment}-administrators"
   description      = "Azure AKS Administrators for ${azurerm_resource_group.aks_rg.name}-${var.environment} cluster"
   security_enabled = true
--- tf-manifests/outputs.tf
+++ tf-manifests/outputs.tf
@@ -1,9 +1,9 @@
 output "location" {
-  value = azurerem_resource_group.aks_rg.location
+  value = azurerm_resource_group.aks_rg.location
 }
 
 output "varsions" {
-  value = data.azurerm_kubernetes_service_versions.current.version
+  value = data.azurerm_kubernetes_service_versions.current.versions
 }
 
 output "latest_version" {
@@ -16,4 +16,16 @@ output "azure_ad_group_id" {
 
 output "azure_ad_group_objectid" {
   value = azuread_group.aks_admins.object_id
+}
+
+output "aks_cluster_id" {
+  value = azurerm_kubernetes_cluster.aks_cluster.id
+}
+
+output "aks_cluster_name" {
+  value = azurerm_kubernetes_cluster.aks_cluster.name
+}
+
+output "aks_cluster_kubernetes_version" {
+  value = azurerm_kubernetes_cluster.aks_cluster.kubernetes_version
 }
