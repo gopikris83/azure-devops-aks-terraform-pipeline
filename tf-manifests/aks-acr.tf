2d17b457ebb3f7516460063b6fba8e06329ee4fd
Gopi Krishna <gopikrishna.83@gmail.com>
2022-03-14 19:33:56 +0100

    Added scope

--- tf-manifests/aks-acr.tf
+++ tf-manifests/aks-acr.tf
@@ -13,3 +13,13 @@ resource "azurerm_container_registry" "aks-acr" {
     Environment = var.environment
   }
 }
+
+resource "azurerm_container_registry_scope_map" "example" {
+  name                    = "aks-scope-map"
+  container_registry_name = azurerm_container_registry.aks-acr.name
+  resource_group_name     = azurerm_resource_group.aks_rg.name
+  actions = [
+    "repositories/repo1/content/read",
+    "repositories/repo1/content/write"
+  ]
+}
